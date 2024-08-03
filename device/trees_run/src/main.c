0b05426191d4e27fd8e1a5322849095038232ae6
Jon Nordby <jononor@gmail.com>
2024-08-03 16:28:11 +0200

    zephyr: Setup USB serial for pico

--- device/trees_run/src/main.c
+++ device/trees_run/src/main.c
@@ -1,30 +1,41 @@
-/*
- * emlearn example for Zephyr RTOS
- */
+
+#include "runtests.c"
 
 #include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/util.h>
+#include <string.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/uart.h>
 
-#include "xor_model.h"
-
-void xor_test(void)
-{
-    const int16_t test_inputs[4][2] = {
-        { 0,    0   },
-        { 255,  0   },
-        { 0,    255 },
-        { 255,  255 },
-    };
-
-    for (int i=0; i<4; i++) {
-        const int16_t *features = test_inputs[i];
-        const int out = xor_model_predict(features, 2);
-
-        printf("xor(%d,%d) = %d\n", (int)features[0], (int)features[1], out);
-    }
-}
+/* Check overlay exists for CDC UART console */ 
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+	    "Console device is not ACM CDC UART device");
 
 int main(void)
 {
-    xor_test();
+	/* Configure to set Console output to USB Serial */ 
+	const struct device *usb_device = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	uint32_t dtr = 0;
+
+    /* Check if USB can be initialised, bails out if fail is returned */
+	if (usb_enable(NULL) != 0) {
+		return;
+	}
+
+	/* Wait for a console connection, if the DTR flag was set to activate USB */
+	while (!dtr) {
+		uart_line_ctrl_get(usb_device, UART_LINE_CTRL_DTR, &dtr);
+		k_sleep(K_MSEC(100));
+	}
+
+	while (1) {
+
+        run_tests();
+		k_sleep(K_MSEC(1000));
+	}
+    
     return 0;
 }
+
+
